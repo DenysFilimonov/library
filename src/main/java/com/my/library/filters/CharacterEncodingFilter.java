60a474148a517ddb0afdf00e0b4ac040cb9184dc
DenisFilimonov <filimonov.ppd@gmail.com>
2023-03-03 16:52:17 +0200

    encoding7

--- src/main/java/com/my/library/filters/CharacterEncodingFilter.java
+++ src/main/java/com/my/library/filters/CharacterEncodingFilter.java
@@ -3,8 +3,10 @@ package com.my.library.filters;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Enumeration;
 
 import static java.nio.charset.StandardCharsets.ISO_8859_1;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -19,6 +21,11 @@ public class CharacterEncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
+        HttpServletRequest req = (HttpServletRequest) request;
+        Enumeration attributeNames = req.getSession().getAttributeNames();
+        while (attributeNames.hasMoreElements())
+            System.out.println(attributeNames.nextElement());
+        System.out.println(req.getSession().getAttribute("javax.servlet.jsp.jstl.fmt.request.charset"));
         chain.doFilter(request, response);
     }
 }

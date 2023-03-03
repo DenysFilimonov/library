6558bfc7dbd94c9b4e7ff9253625cc7b2e414e70
DenisFilimonov <filimonov.ppd@gmail.com>
2023-03-03 15:36:22 +0200

    encoding4

--- src/main/java/com/my/library/services/MailManager.java
+++ src/main/java/com/my/library/services/MailManager.java
@@ -6,16 +6,14 @@ import org.apache.commons.compress.utils.IOUtils;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
 import java.util.Date;
+import java.util.Enumeration;
 import java.util.Properties;
 import java.util.stream.Stream;
 
@@ -67,18 +65,53 @@ public class MailManager {
     public static void main(String[] args) throws IOException {
         //InputStream in = new Entity().getClass().getResourceAsStream("/text_ua.properties");
         File targetFile = new File("src/main/resources/text_ua_2.properties");
-        File sourceFile = new File("src/main/resources/text_ua.properties");
-        try (Stream<String> lines =
-                     Files.lines(sourceFile.toPath(), StandardCharsets.UTF_8)) {
+        FileWriter writer = new FileWriter("src/main/resources/text_ua_2.properties", true);
+        //File sourceFile = new File("src/main/resources/text_ua.properties");
+        //try (Stream<String> lines =
+ //                    Files.lines(sourceFile.toPath(), StandardCharsets.UTF_8)) {
+//
+   //         lines.forEach(line -> {
+  //            String convertLine = new String(line.getBytes(Charset.forName("UTF-8")), Charset.forName("UTF-8"));
+      //          System.out.println(convertLine);
+   //         });
 
-            lines.forEach(line -> {
-                String convertLine = new String(line.getBytes(Charset.forName("UTF-8")), Charset.forName("UTF-8"));
-                System.out.println(convertLine);
-            });
-
+        InputStream in;
+        in = new Entity().getClass().getResourceAsStream("/text_ua_2.properties");
+        Properties prop = new Properties();
+        prop.load(in);
+        Enumeration properties = prop.keys();
+        while(properties.hasMoreElements()){
+            String key = (String) properties.nextElement();
+            String property = (String) prop.get(key);
+            System.out.println(property);
+            System.out.println(charset(property, new String[] {"ISO-8859-1"}));
+            String encodedProperty =new String(property.getBytes(Charset.forName("ISO-8859-1")), Charset.forName("UTF-8"));
+            System.out.println(key+"="+encodedProperty);
+            //writer.write(key+"="+encodedProperty+"\n");
         }
-       // String subj = new String(subject.getBytes(Charset.forName("ISO-8859-1")), Charset.forName("UTF-8"));
+
+
+
     }
+
+    public static String convert(String value, String fromEncoding, String toEncoding) throws UnsupportedEncodingException {
+        return new String(value.getBytes(fromEncoding), toEncoding);
+    }
+
+    public static String charset(String value, String charsets[]) throws UnsupportedEncodingException {
+        String probe = StandardCharsets.UTF_8.name();
+        for(String c : charsets) {
+            Charset charset = Charset.forName(c);
+            if(charset != null) {
+                if(value.equals(convert(convert(value, charset.name(), probe), probe, charset.name()))) {
+                    return c;
+                }
+            }
+        }
+        return StandardCharsets.UTF_8.name();
+    }
+
+       // String subj = new String(subject.getBytes(Charset.forName("ISO-8859-1")), Charset.forName("UTF-8"));
 
 
 }
